9ac9ce1bceb32d1521851ec69084e2e4be2b4aa8
Clemens <dubcmsky@protonmail.com>
2022-03-19 22:12:24 +0100

    added User RestAPI Endpoints

--- ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserController.java
+++ ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserController.java
@@ -1,4 +1,38 @@
 package com.dubsky.cardgame.apiserver.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
+
+/**
+ * @author Dubsky
+ * @version 1.0
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api/user")
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    /**
+     * POST-Method to create a new user
+     *
+     * @param key API-Key for authentication
+     * @param username Username for the account to be created
+     * @param password Password for the account to be created
+     */
+    @PostMapping(path = "/newUser")
+    public void newUser(
+            @RequestParam String key,
+            @RequestParam String username,
+            @RequestParam String password) {
+        System.out.println("[POST] New User: " + username);
+    }
 }
--- ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserRepository.java
+++ ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserRepository.java
@@ -1,4 +1,12 @@
 package com.dubsky.cardgame.apiserver.User;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Dubsky
+ * @version 1.0
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
