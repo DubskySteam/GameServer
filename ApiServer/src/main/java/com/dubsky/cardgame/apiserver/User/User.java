5ddfaf4875a9ba7b8c147db634bee88a930b384a
Clemens <dubcmsky@protonmail.com>
2022-03-19 22:06:58 +0100

    aded User DB model

--- ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/User.java
+++ ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/User.java
@@ -1,4 +1,47 @@
 package com.dubsky.cardgame.apiserver.User;
 
+import com.sun.istack.NotNull;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+/**
+ * @author Dubsky
+ * @version 1.0
+ */
+@Data
+@Entity
+@Table(name="user")
 public class User {
+
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @NotNull
+    private String username;
+    @NotNull
+    private String password;
+    @NotNull
+    private String email;
+
+    public User() {
+    }
+
+    public User(int id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(int id, String username, String password, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
 }
