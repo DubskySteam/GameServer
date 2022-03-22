89da024ac5013e0e55e5a44213307a65b6fea05e
Clemens <dubcmsky@protonmail.com>
2022-03-22 23:35:03 +0100

    addded user api change calls

--- ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserController.java
+++ ApiServer/src/main/java/com/dubsky/cardgame/apiserver/User/UserController.java
@@ -4,10 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * @author Dubsky
- * @version 1.0
+ * @version 1.1
  */
 @RestController
 @CrossOrigin(origins = "*")
@@ -34,7 +35,7 @@ public class UserController {
             @RequestParam String key,
             @RequestParam String username,
             @RequestParam String password) {
-        System.out.format("[GET] Login attempt: " + username);
+        System.out.format("[GET] Login attempt >> " + username);
             User tmpUser = userRepository.login(username, password);
             if (tmpUser != null
                     && tmpUser.getUsername().equals(username)
@@ -56,7 +57,46 @@ public class UserController {
             @RequestParam String key,
             @RequestParam String username,
             @RequestParam String password) {
-        System.out.println("[POST] New User: " + username);
+        System.out.println("[POST] New User >> " + username);
         userRepository.save(new User(username, password));
+    }
+
+    /**
+     * POST-Method to a users email
+     *
+     * @param key API-Key for authentication
+     * @param id id of the user
+     * @param email Email to be changed to     */
+    @PostMapping(path = "/changeEmail")
+    public void changeEmail(
+            @RequestParam String key,
+            @RequestParam int id,
+            @RequestParam String email) {
+        System.out.println("[POST] Change User(id="+id+") Email to >> " + email );
+        Optional<User> tmp = userRepository.findById(id);
+        if(tmp.isPresent()) {
+            tmp.get().setEmail(email);
+            userRepository.save(tmp.get());
+        }
+    }
+
+    /**
+     * POST-Method to a users email
+     *
+     * @param key API-Key for authentication
+     * @param id id of the user
+     * @param password new password
+     */
+    @PostMapping(path = "/changePassword")
+    public void changePassword(
+            @RequestParam String key,
+            @RequestParam int id,
+            @RequestParam String password) {
+        System.out.println("[POST] Change User(id="+id+") Password to >> " + password );
+        Optional<User> tmp = userRepository.findById(id);
+        if(tmp.isPresent()) {
+            tmp.get().setPassword(password);
+            userRepository.save(tmp.get());
+        }
     }
 }
